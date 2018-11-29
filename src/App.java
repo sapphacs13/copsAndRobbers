fdcd0013b3b8b177c0da37515da5945fb6342211
sapphacs13 <sappha.omeara@gmail.com>
2018-11-29 17:56:19 -0500

    init commit

---
+++ src/App.java
@@ -0,0 +1,28 @@
+import org.jgrapht.*;
+import org.jgrapht.graph.*;
+import org.jgrapht.io.*;
+import org.jgrapht.traverse.*;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+public class App {
+    public static void main(String[] args){
+        Graph<URL, DefaultEdge> g = new DefaultDirectedGraph<>(DefaultEdge.class);
+
+       /* URL google = new URL("http://www.google.com");
+        URL wikipedia = new URL("http://www.wikipedia.org");
+        URL jgrapht = new URL("http://www.jgrapht.org");
+
+        // add the vertices
+        g.addVertex(google);
+        g.addVertex(wikipedia);
+        g.addVertex(jgrapht);
+
+        // add edges to create linking structure
+        g.addEdge(jgrapht, wikipedia);
+        g.addEdge(google, jgrapht);
+        g.addEdge(google, wikipedia);
+        g.addEdge(wikipedia, google);*/
+    }
+}
